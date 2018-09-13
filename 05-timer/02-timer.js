d0b47fb10c7795541fc7054c34d12bdbdf04fd65
liuxiumei123 <991955909@qq.com>
2018-09-13 08:29:23 +0800

    第二次任务

--- 05-timer/02-timer.js
+++ 05-timer/02-timer.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 
 var timeID=setInterval(loop,1000);
+var count=0; 
 
 function loop(){
   count++;
   if(count===10){
     clearInterval(timeID);
+    //timeID.unref();
   }
   console.log("I will running forever!");
 }
 
-timeID.unref();
+
+console.log(timeID);
+console.log(typeof timeID);
+
