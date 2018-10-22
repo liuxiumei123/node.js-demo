98c84addf6ff517d5eb072a52ba0854f4adcb582
wangding <408542507@qq.com>
2018-10-22 09:44:06 +0800

    任务提交

--- 11-debug/server.js
+++ 11-debug/server.js
@@ -4,53 +4,53 @@
 var http = require('http'),
     fs = require('fs'),
     path = require('path'),
-   // mime = require('mime'),
+    // mime = require('mime'),
     cache = {};
 
 http.createServer(function(req, res) {
-  var url = 'http://' + req.headers.host + req.url;
+    var url = 'http://' + req.headers.host + req.url;
 
-  console.log('URL:', url);
-  console.log(req.headers);
-  console.log('');
+    console.log('URL:', url);
+    console.log(req.headers);
+    console.log('');
 
-  var fileName = (req.url === '/') ? path.join(__dirname, 'www/index.html') : path.join(__dirname, 'www', req.url);
-  console.log('fileName:', fileName);
+    var fileName = (req.url === '/') ? path.join(__dirname, 'www/index.html') : path.join(__dirname, 'www', req.url);
+    console.log('fileName:', fileName);
 
-  fs.stat(fileName, function(err, stat) {
-    if(err) {
-      if('ENOENT' === err.code) {
-        send404(res);
-      } else {
-        res.statusCode = 500;
-        res.end(err.message);
-      }
+    fs.stat(fileName, function(err, stat) {
+        if(err) {
+            if('ENOENT' === err.code) {
+                send404(res);
+        } else {
+            res.statusCode = 500;
+            res.end(err.message);
+        }
     } else {
-      console.log(cache);
-      if(cache[fileName]) {
-        sendFile(res. fileName, cache[fileName]);
-      } else {
-        cache[fileName] = fs.readFileSync(fileName);
-        sendFile(res, fileName, cache[fileName]);
-      }
+        console.log(cache);
+        if(cache[fileName]) {
+            sendFile(res. fileName, cache[fileName]);
+        } else {
+            cache[fileName] = fs.readFileSync(fileName);
+            sendFile(res, fileName, cache[fileName]);
+        }
     }
   });
 }).listen(8000);
 
 function send404(res) {
-  res.writeHead(404, { 'Content-Type': 'text/plain' });
-  res.end('Error 404: resource not found.');
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Error 404: resource not found.');
 }
 
 function sendFile(res, file, fileContents) {
-  console.log('sendFile function, file:', file);
-  console.log('sendFile function, fileContents:', fileContents);
+    console.log('sendFile function, file:', file);
+    console.log('sendFile function, fileContents:', fileContents);
 
-  res.writeHead(200, {
-    'Content-Type': mime.getType(path.basename(file)),
-    'Content-Length': Buffer.byteLength(fileContents)
+    res.writeHead(200, {
+      'Content-Type': mime.getType(path.basename(file)),
+      'Content-Length': Buffer.byteLength(fileContents)
   });
   
-  res.end(fileContents);
+    res.end(fileContents);
 }
 
