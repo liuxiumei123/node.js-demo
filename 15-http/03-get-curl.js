65f4f25cda79f59b693237d72c2e8344362f5ce1
wangding <408542507@qq.com>
2018-10-31 13:54:00 +0800

    任务提交

--- 15-http/03-get-curl.js
+++ 15-http/03-get-curl.js
@@ -6,9 +6,12 @@ const  URL=require('url');
 //var dir="http://user:pass@host.com:8080/p/a/t/h?query=string#hash";
 //console.log(url.parse(dir,true,true));
 
-var city=process.argv[2]||'石家庄';
+//var city=process.argv[2]||'石家庄';
+//Node 默认不支持中文编码,所以在生成url参数的时候  使用如下方法进行处理
+var city=encodeURIComponent(process.argv[2]||'石家庄');
 
-var addr="http://v.juhe.cn/historyWeather/citys?key=2524407f6e43e074fe7df33456223436&province_id=15";
+//var addr="http://v.juhe.cn/historyWeather/citys?key=2524407f6e43e074fe7df33456223436&province_id=15";  //历史记录
+var addr ="http://v.juhe.cn/weather/index?format=2&cityname="+city+"&key=59cf6410bb5a22dd03006aba831a0fe6";//当天天气
 
 
 http.get(addr,res=>{
