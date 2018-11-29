5abd95b65f8900b7d80c5f4ad5978eb3143d0f16
wangding <408542507@qq.com>
2018-11-29 09:13:36 +0800

    任务提交

--- 01-introduction/01-hello-world.js
+++ 01-introduction/01-hello-world.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
+var http=require('http');
+var fs=require('fs');
+
+
+http.createServer(function(req,res){
+ // res.setHeader('Access-Control-Allow-Origin',"*");
+ var data= fs.readFileSync('dog.jpg');
+ res.end(data);
+}).listen(8080);
 
 console.log("hello world");
 
--- express-demo/server.js
+++ express-demo/server.js
@@ -2,25 +2,44 @@
 
 const bodyParser=require('body-parser');
 const formidable=require('formidable');
+const fs=require('fs');
+const path=require('path');
 const express=require('express');
 const app=express();
 app.listen(8080);
-var path=require('path');
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser());
-
+app.get('/',function(req,res){
+  res.sendFile('index.html')
+})
 
 app.post('/upload',function(req,res){
   console.log(req.body);
   console.log('');
   var form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files){
+  form.uploadDir = "./upload";
+  
+  form.on('file', function(name, files) {
+      console.log('');
+      console.log(name,files);
+      console.log('');
+    var oldpath =__dirname + "/"+ files.path;
+    var path = require("path");
+    var extname = path.extname(files.name);
+    var newpath = __dirname + "/upload/"+files.name;
+    fs.rename(oldpath,newpath,function (err) {
+      if(err){
+         throw Error("改名失败");                             
+      }
+     res.end("success");  
+
+    }); 
+  });
+
+
+  form.parse(req, function(err, fields){
     if(err) return res.end('error');
-    console.log('received fields:');
     console.log(fields);
-    console.log('received files:');
-    console.log(files);
-    res.end('hello world');
-  })
+  });
 })
